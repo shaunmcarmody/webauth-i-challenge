429a331a2a05c94f5559faec2dd44d63930f988c
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-01 20:44:16 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const users = require('./controllers/users/users.routes.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use('/api/', users);
+
+server.listen(5000, console.log('Listening on Port 5000'));
