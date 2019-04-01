2eb8d9f0d76eac279db8e2b518c0f7ec8292bce1
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-01 21:58:07 +0100

    register user on request

--- controllers/users/users.model.js
+++ controllers/users/users.model.js
@@ -1,9 +1,18 @@
 const db = require('../../data/dbConfig.js');
 
+const returnUser = id =>
+  db('users')
+    .where('id', id)
+    .select('username')
+    .first()
 
+const insertUser = user =>
+  db('users')
+    .insert(user);
 
 
 
 module.exports = {
-  
+  returnUser,
+  insertUser
 }
--- controllers/users/users.routes.js
+++ controllers/users/users.routes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const db = require('./users.model');
 
+router.post('/register', async (req, res, next) => {
+  // Username and Password required
+  req.body.password = bcrypt.hashSync(req.body.password, 4);
+  try {
+    const result = await db.insertUser(req.body);
+    const { username } = await db.returnUser(result[0]);
+    res.status(200).json({ message: `Welcome ${username}! You've registered successfully` })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/login', (req, res, next) => {
+  // Username and Password required
+});
+
+router.get('/users', (req, res, next) => {
+  // Return users if logged in
+  // 'You shall not pass!' if not logged in
+});
 
 
 
